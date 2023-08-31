fdd5769bfd7f06ad12ecf6291c8b0d02c3e1d7af
Anastasiya <agropina_90@mail.ru>
2023-08-31 15:49:57 +0300

    Перевернуть предложение

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,49 +7,37 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(getGrade(95,90,93));
-        System.out.println("result = " + "А");
-
-        System.out.println("");
-
-        System.out.println(getGrade(60,82,76));
-        System.out.println("result = " + "С");
-
-        System.out.println("");
-
-        System.out.println(getGrade(1,2,6));
-        System.out.println("result = " + "F");
-
-
-
+        System.out.println(reverseWords("hello world"));
+        System.out.println("result = " + "world hello");
+//
 //        System.out.println("");
 //
-//        System.out.println(rps("rock", "rock"));
-//        System.out.println("result = " + "Draw!");
+//        System.out.println(getGrade(60,82,76));
+//        System.out.println("result = " + "С");
+//
+//        System.out.println("");
+//
+//        System.out.println(getGrade(1,2,6));
+//        System.out.println("result = " + "F");
+
+
 
 
     }
 
-    public static char getGrade(int s1, int s2, int s3) {
+    public static String reverseWords(String str) {
 
-        double average = IntStream.of(s1, s2, s3).average().getAsDouble();
-
-        if (isBetween(average, 90, 101)) return 'A';
-        if (isBetween(average, 80, 90)) return 'B';
-        if (isBetween(average, 70, 80)) return 'C';
-        if (isBetween(average, 60, 70)) return 'D';
-        else return 'F';
+//        String temp = "";
+//
+//        String[] arr = str.split(" ");
+//
+//        for (int i = arr.length-1;  i >= 0 ; i--) {
+//
+//            temp = temp + arr[i] + " ";
+//        }
+//
+//        return temp.trim();
+        return Arrays.stream(str.split(" ")).reduce((x, y) -> y + " " + x).get();
 
     }
-
-    public static boolean isBetween(double x, int lower, int upper) {
-        return lower <= x && x < upper;
-    }
-
-
 }
-/*
-"scissors", "paper" --> "Player 1 won!"
-"scissors", "rock" --> "Player 2 won!"
-"paper", "paper" --> "Draw!"
-* */
